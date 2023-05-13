0a2459561885e4241c0c7fcef7a43edde44cf985
elik <lavriket@gmail.com>
2023-05-13 15:29:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val getBack = findViewById<TextView>(R.id.arrowID)
+        getBack.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+    }
+}
